e8d55a923b4974862fe0fed840a9b317f87e7eff
Akhil V <akhil.v@flycatchtech.com>
2025-02-06 15:02:36 +0530

    feat(flatseal): add flatseal installation

--- installation-script.sh
+++ installation-script.sh
@@ -56,6 +56,7 @@ flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub io.dbeaver.DBeaverCommunity
 flatpak install -y flathub com.getpostman.Postman
 flatpak install -y flathub com.obsproject.Studio
+flatpak install -y flathub com.github.tchx84.Flatseal
 
 echo "Base software installation completed. Please restart your system."
 
